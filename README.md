d5f7d45d4fa289953bf64a72357416ae78040eab
ZhaoKe <1355449245@qq.com>
2024-03-30 21:16:25 +0800

    update function

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 A library of simulation calculation tools for quantitative genetics developed for poultry breeding.
 
+### Key Issue
+The core of the entire algorithm consists of two parts, how to calculate the kinship correlation coefficient, and how to assign female individuals to male poultry. 
+In fact, our algorithm should be expressed in another way, how to select male individuals for female poultry. 
+Because there is a one-to-many relationship between males and females, it is actually critical which one is dominant in the algorithm.
+
+
+### File Structure
 ```text
 root(Sound-Representation-KZ)
 └─Breeding_Main.py  // Main Program
--- breedingkits.py
+++ breedingkits.py
@@ -4,5 +4,28 @@
 # @Author: ZhaoKe
 # @File : breedingkits.py
 # @Software: PyCharm
+import numpy as np
 
 
+def calculate_kinship_matrix(population):
+    male_list = []
+    female_list = []
+    male_cnt, female_cnt = 0, 0
+    for item in population:
+        if item.sex == 1:
+            male_cnt += 1
+            male_list.append(item)
+        else:
+            female_cnt += 1
+            female_list.append(item)
+    kinship_matrix = np.zeros((male_cnt, female_cnt))
+    for i, item_m in enumerate(male_list):
+        for j, item_f in enumerate(female_list):
+            kinship_matrix[i, j] = calculate_kin_correlation_coef(item_m, item_f)
+
+
+def calculate_kin_correlation_coef(fa, ma):
+    if fa.ge_idx < 1 and ma.ge_idx < 1:
+        return 0
+    else:
+        return -1
